8d510e70503f70c03837fcd8c662f449ee01cb81
Samuel Alev <samuel.alev@protonmail.com>
2023-04-05 22:03:03 +0200

    test: add test

--- apps/accordion-block/tests/AccordionBlock.spec.tsx
+++ apps/accordion-block/tests/AccordionBlock.spec.tsx
@@ -10,7 +10,6 @@ const ACCORDION_ITEM_HEADING_TEST_ID = 'accordion-item-heading';
 const ACCORDION_ITEM_CONTENT_TEST_ID = 'accordion-item-content';
 const ACCORDION_ITEM_TRIGGER_TEST_ID = 'accordion-item-trigger';
 const ACCORDION_ITEM_TEST_ID = 'accordion-item-';
-const ACCORDION_EMPTY_ITEM_TEST_ID = 'accordion-item-empty';
 
 const ACCORDION_ITEMS: BlockSettings['accordionItems'] = [
     {
@@ -25,16 +24,21 @@ const ACCORDION_ITEMS: BlockSettings['accordionItems'] = [
     },
 ];
 
+const DEFAULT_DATE = new Date(2023, 2, 15);
+
 describe('Accordion Block', () => {
     beforeEach(() => {
-        vi.useFakeTimers();
+        // Uses fake timer on Date so the empty accordion item uses the same timestamp
+        vi.useFakeTimers({ toFake: ['Date'] });
+        vi.setSystemTime(DEFAULT_DATE);
     });
 
     afterEach(() => {
+        vi.runOnlyPendingTimers();
         vi.useRealTimers();
     });
 
-    it('renders an empty accordion block', () => {
+    it('renders the block', () => {
         const [AccordionBlockWithStubs] = withAppBridgeBlockStubs(AccordionBlock);
         const { getByTestId } = render(<AccordionBlockWithStubs />);
 
@@ -42,15 +46,12 @@ describe('Accordion Block', () => {
     });
 
     it('renders an empty accordion when no settings and in edit mode', () => {
-        const date = new Date(2023, 2, 15);
-        vi.setSystemTime(date);
-
         const [AccordionBlockWithStubs] = withAppBridgeBlockStubs(AccordionBlock, {
             editorState: true,
         });
         const { getByTestId } = render(<AccordionBlockWithStubs />);
 
-        expect(getByTestId(`${ACCORDION_ITEM_TEST_ID}${date.getTime()}`)).toBeTruthy();
+        expect(getByTestId(`${ACCORDION_ITEM_TEST_ID}${DEFAULT_DATE.getTime()}`)).toBeTruthy();
     });
 
     it('renders nothing when no settings and in preview mode', () => {
@@ -58,10 +59,10 @@ describe('Accordion Block', () => {
         const { getByTestId, queryByTestId } = render(<AccordionBlockWithStubs />);
 
         expect(getByTestId(ACCORDION_BLOCK_TEST_ID)).toBeTruthy();
-        expect(queryByTestId(ACCORDION_EMPTY_ITEM_TEST_ID)).toBeFalsy();
+        expect(queryByTestId(`${ACCORDION_ITEM_TEST_ID}${DEFAULT_DATE.getTime()}`)).toBeFalsy();
     });
 
-    it.skip('renders an accordion block with content', async () => {
+    it('renders an accordion block with content', async () => {
         const [AccordionBlockWithStubs] = withAppBridgeBlockStubs(AccordionBlock, {
             blockSettings: {
                 accordionMultiple: true,
@@ -89,26 +90,26 @@ describe('Accordion Block', () => {
     });
 
     it.skip('renders an empty accordion item in edit mode if the last item has heading text', () => {
-        // noop
+        // TODO
     });
 
     it.skip('renders an empty accordion item in edit mode if the last item has content', () => {
-        // noop
+        // TODO
     });
 
     it.skip('does not render a second empty accordion item if the last item is empty', () => {
-        // noop
+        // TODO
     });
 
     it.skip('does not render an empty accordion item in preview mode', () => {
-        // noop
+        // TODO
     });
 
     it.skip('only open one accordion item at a time', () => {
-        // noop
+        // TODO
     });
 
     it.skip('opens multiple accordion item at the same time', () => {
-        // noop
+        // TODO
     });
 });
