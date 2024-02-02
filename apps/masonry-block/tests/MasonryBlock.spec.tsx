4d01e340d49b4862acf050e8ce32a67b0becac4c
Samuel Alev <samuel.alev@frontify.com>
2024-02-02 16:40:18 +0100

    fix: format issue

--- apps/masonry-block/tests/MasonryBlock.spec.tsx
+++ apps/masonry-block/tests/MasonryBlock.spec.tsx
@@ -10,7 +10,6 @@ describe.skip('Masonry Block', () => {
     // it('renders the block', () => {
     //     const [MasonryBlockWithStubs] = withAppBridgeBlockStubs(MasonryBlock);
     //     const { getByTestId } = render(<MasonryBlockWithStubs />);
-
     //     expect(getByTestId(MASONRY_BLOCK_TEST_ID)).toBeTruthy();
     // });
 });
