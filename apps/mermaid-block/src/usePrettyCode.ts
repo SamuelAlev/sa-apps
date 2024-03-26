e7f89f7d53b06a4209feaf1b1ce3a6c1cf8aefae
Samuel Alev <samuel.alev@frontify.com>
2024-03-26 23:26:34 +0100

    feat: improve mermaid block deps loading

--- apps/mermaid-block/src/usePrettyCode.ts
+++ apps/mermaid-block/src/usePrettyCode.ts
@@ -1,5 +1,7 @@
 import { useEffect, useRef, useState } from 'react';
-import type { ShikiCodeToHtml } from './types';
+import type { ShikiCodeToHtml } from 'shiki';
+
+import packageJson from '../package.json';
 
 export const usePrettyCode = (code: string, options: { language: string; theme: string } = { language: 'mermaid', theme: 'github-dark' }) => {
     const shikiCodeToHtml = useRef<ShikiCodeToHtml | null>(null);
@@ -8,14 +10,29 @@ export const usePrettyCode = (code: string, options: { language: string; theme: 
     useEffect(() => {
         const renderPrettyCode = async () => {
             // @ts-expect-error Loading from CDN with treeshaking
-            shikiCodeToHtml.current ??= await import('https://esm.sh/shiki@^1.0.0').then((mod) => mod.codeToHtml);
+            shikiCodeToHtml.current ??= await import(/* @vite-ignore */ `https://esm.sh/shiki@${packageJson.dependencies.shiki}`).then((mod) => mod.codeToHtml);
             if (shikiCodeToHtml.current) {
                 const renderedCode = await shikiCodeToHtml.current(`\`\`\`mermaid\n${code}\n\`\`\``, {
                     lang: options.language,
                     theme: options.theme,
                 });
 
-                setPrettyCode(renderedCode);
+                // Remove first and last line with "```mermaid", "```" and the 2 `\n`, because Shiki needs them to detect the language
+                const codeElement = document.createElement('div');
+                codeElement.innerHTML = renderedCode;
+
+                const lineClasses = codeElement.querySelectorAll('.line');
+
+                if (lineClasses.length > 0) {
+                    lineClasses[0].parentNode?.removeChild(lineClasses[0]);
+                    lineClasses[lineClasses.length - 1].parentNode?.removeChild(lineClasses[lineClasses.length - 1]);
+                }
+
+                const codeTag = codeElement.querySelector('code');
+                codeTag?.childNodes[0].remove();
+                codeTag?.childNodes[codeTag.childNodes.length - 1].remove();
+
+                setPrettyCode(codeElement.innerHTML);
             }
         };
 
