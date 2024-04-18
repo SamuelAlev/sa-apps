624255422546a21e06a88ebece084aea83215a02
Samuel Alev <samuel.alev@protonmail.com>
2024-04-18 22:47:31 +0200

    fix: remove the "use client" for now

--- packages/tooltip/src/Tooltip.tsx
+++ packages/tooltip/src/Tooltip.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import * as TooltipPrimitive from '@radix-ui/react-tooltip';
 import { cn } from '@sa-apps/utilities';
 import { type ComponentPropsWithoutRef, type ElementRef, forwardRef } from 'react';
