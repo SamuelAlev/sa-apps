0b83f55539bd50efa3b508edf94880f7197683c5
Samuel Alev <samuel.alev@frontify.com>
2024-11-06 10:58:43 +0100

    fix: linting

--- packages/tracking/src/withTracking.tsx
+++ packages/tracking/src/withTracking.tsx
@@ -42,7 +42,7 @@ export const withTracking =
 
             useEffect(() => {
                 if (loaded && websiteId) {
-                    store.websiteId = import.meta.env.PROD ? websiteId ?? '' : WEBSITE_ID_DEV;
+                    store.websiteId = import.meta.env.PROD ? (websiteId ?? '') : WEBSITE_ID_DEV;
                     window.umami?.track((props) => ({ ...props, website: store.websiteId }));
                 }
             }, [websiteId, loaded]);
